d8ae0f610009f2bb307a23799399c442b39dff59
Tomislav Obajdin <tobajdin12@gmail.com>
2024-09-05 21:11:10 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -120,7 +120,7 @@ function Navline() {
           <img alt="#" src="img/house-search-svgrepo-com.svg"></img>
           <div className="logoText">
             <p>
-              <b>Peacse</b> Of
+              <b>Peace</b> Of
             </p>
             <p>Mind</p>
           </div>
